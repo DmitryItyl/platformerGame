35251b766d492ee4d018807d73b9c2caa0d1617e
Dmitry B <theodridspm@gmail.com>
2022-02-13 20:42:12 +0200

    Added score board top 10

--- Assets/Scripts/LeaderBoard.cs
+++ Assets/Scripts/LeaderBoard.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeaderBoard : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        List<LeaderBoardEntry> highScoresList = FileManager.ReadFromJSON<LeaderBoardEntry>(FileManager.highScoreFileName);
+        for (int i = 0; i < highScoresList.Count; i++)
+        {
+            LeaderBoardEntry highScore = highScoresList[i];
+            Transform entry = transform.Find(string.Format("highscoreEntry{0}", i + 1));
+            Text entryName = entry.Find("nameText").GetComponent<Text>();
+            Text entryScore = entry.Find("scoreText").GetComponent<Text>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            entryName.text = highScore.playerName;
+            entryScore.text = highScore.score.ToString();
+        }
     }
 }
--- Assets/Scripts/WinSceneManager.cs
+++ Assets/Scripts/WinSceneManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,11 +10,51 @@ public class WinSceneManager : MonoBehaviour
     [SerializeField] InputField nameField;
     [SerializeField] Text scoreText;
 
+    const int maximumLeaderboardEntries = 10;
+
 
     // Start is called before the first frame update
     void Start()
     {
         nameField.text = PlayerPrefs.GetString("Name");
         scoreText.text = ScoreManager.totalScore.ToString();
+    }
+
+    public void SavePlayerResult()
+    {
+        string playerName = nameField.text;
+        int playerScore = ScoreManager.totalScore;
+
+        PlayerPrefs.SetString("Name", playerName);
+        PlayerPrefs.SetInt("Level", 1);
+        ScoreManager.ResetScore();
+
+        UpdateLeaderBoard(playerName, playerScore);
+    }
+
+    void UpdateLeaderBoard(string playerName, int score)
+    {
+        List<LeaderBoardEntry> leaderBoard = FileManager.ReadFromJSON<LeaderBoardEntry>(FileManager.highScoreFileName);
+
+        LeaderBoardEntry newLeaderBoardEntry = new LeaderBoardEntry(playerName, score);
+        leaderBoard.Add(newLeaderBoardEntry);
+        leaderBoard = leaderBoard.OrderByDescending(x => x.score).ToList();
+        if (leaderBoard.Count > maximumLeaderboardEntries)
+            leaderBoard.RemoveAt(maximumLeaderboardEntries);
+
+        FileManager.SaveToJSON<LeaderBoardEntry>(leaderBoard, FileManager.highScoreFileName);
+    }
+}
+
+[Serializable]
+class LeaderBoardEntry
+{
+    public string playerName;
+    public int score;
+
+    public LeaderBoardEntry(string name, int score) 
+    {
+        this.score = score;
+        this.playerName = name;
     }
 }
